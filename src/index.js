2a84acdaae61c4621fe2bb0cc2ce6d72737c73f4
therunningagent <luistjr@gmail.com>
2020-12-22 10:27:08 -0500

    deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const taskButton = document.querySelector("#create-task-form")
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority")
+
+  
+  
+  const createNewTask = event => {
+    event.preventDefault();
+
+    const newTaskDescription = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = newTaskDescription.value; 
+
+    appendNewTask(newTask);
+    event.target.reset();
+
+    console.log(newTask)
+  }
+
+  taskButton.addEventListener("submit", createNewTask)
+
+  const appendNewTask = task => {
+    document.getElementById("tasks").appendChild(task);
+  }
+
+
 });
